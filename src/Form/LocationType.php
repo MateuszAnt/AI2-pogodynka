e336ff192409a578f2db23afca21795db10cdbf1
am53731 <am53731@zut.edu.pl>
2025-10-22 11:38:01 +0200

    lab D dokonczyc, skonczone na dodowania routingu do location i measurement

---
+++ src/Form/LocationType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Location;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LocationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('city', null, [
+                'attr' => [
+                    'placeholder' => 'Enter city name',
+                ]
+            ])
+            ->add('latitude', NumberType::class, [
+
+            ])
+            ->add('longitude', NumberType::class, [
+
+            ])
+            ->add('country', ChoiceType::class, [
+                'choices' => [
+                    'Poland' => 'PL',
+                    'Germany' => 'DE',
+                    'France' => 'FR',
+                    'Spain' => 'ES',
+                    'Italy' => 'IT',
+                    'United Kingdow' => 'GB',
+                    'United States' => 'US'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Location::class,
+        ]);
+    }
+}
